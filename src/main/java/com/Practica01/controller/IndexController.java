cc14cf927507597af018fcae42ca45eb8bc7836a
edwin <edwin@192.168.0.16>
2023-02-27 20:31:00 -0600

    Avance de tarea_2 BD

--- src/main/java/com/Practica01/controller/IndexController.java
+++ src/main/java/com/Practica01/controller/IndexController.java
@@ -4,19 +4,43 @@
  */
 package com.Practica01.controller;
 
+import com.Practica01.dao.EstadoDao;
+import com.Practica01.domain.estado;
+import java.util.Arrays;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
 public class IndexController {
     
-   @RequestMapping("/url")
+     @Autowired
+    EstadoDao estadoDao;
+     
+   @GetMapping("/")
     public String page(Model model) {
-        model.addAttribute("attribute", "value");
-        return "view.name";
+      
+   //     log.info("Ahora desde MVC");
+   //     model.addAttribute("Mensaje", "Hola desde el contralodor");
+   //    
+   //     estado Estado = new estado(Long.MIN_VALUE, "Panama", "cuidad Panama", "Los Santos", "4000000", "Si");
+   //     model.addAttribute("Estado", Estado);
+   //     
+   //    
+   //    List<estado> estado = Arrays.asList(Estado);
+   
+        var estado = estadoDao.findAll();
+        model.addAttribute("Estado", estado);
+        
+        return "index";
+        
+        
+        
+        
     }
     
 }
